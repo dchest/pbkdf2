7fee1306835a9c8868ac91ce86eb34d9c676af46
Dmitry Chestnykh <dmitry@codingrobots.com>
2011-04-05 11:35:21 +0200

    Add test vectors for PBKDF2-HMAC-SHA256.

--- pbkdf2_test.go
+++ pbkdf2_test.go
@@ -3,16 +3,20 @@ package pbkdf2
 import (
 	"bytes"
 	"crypto/sha1"
+	"crypto/sha256"
+	"hash"
 	"testing"
 )
 
-// Test vectors from http://tools.ietf.org/html/rfc6070
-var testVectors = []struct {
+// Test vectors from RFC 6070, http://tools.ietf.org/html/rfc6070
+type testVector struct {
 	password string
 	salt     string
 	iter     int
 	output   []byte
-}{
+}
+
+var sha1TestVectors = []testVector{
 	{
 		"password",
 		"salt",
@@ -52,12 +56,50 @@ var testVectors = []struct {
 	},
 }
 
+// Test vectors from http://stackoverflow.com/questions/5130513/pbkdf2-hmac-sha2-test-vectors
+var sha256TestVectors = []testVector{
+	{
+		"password",
+		"salt",
+		1,
+		[]byte{0x12,0x0f,0xb6,0xcf,0xfc,0xf8,0xb3,0x2c,0x43,0xe7,0x22,0x52,0x56,0xc4,0xf8,0x37,0xa8,0x65,0x48,0xc9},
+	},
+	{
+		"password",
+		"salt",
+		2,
+		[]byte{0xae,0x4d,0x0c,0x95,0xaf,0x6b,0x46,0xd3,0x2d,0x0a,0xdf,0xf9,0x28,0xf0,0x6d,0xd0,0x2a,0x30,0x3f,0x8e},
+	},
+	{
+		"password",
+		"salt",
+		4096,
+		[]byte{0xc5,0xe4,0x78,0xd5,0x92,0x88,0xc8,0x41,0xaa,0x53,0x0d,0xb6,0x84,0x5c,0x4c,0x8d,0x96,0x28,0x93,0xa0},
+	},
+	{
+		"passwordPASSWORDpassword",
+		"saltSALTsaltSALTsaltSALTsaltSALTsalt",
+		4096,
+		[]byte{0x34,0x8c,0x89,0xdb,0xcb,0xd3,0x2b,0x2f,0x32,0xd8,0x14,0xb8,0x11,0x6e,0x84,0xcf,0x2b,0x17,0x34,0x7e,0xbc,0x18,0x00,0x18,0x1c},
+	},
+	{
+		"pass\000word",
+		"sa\000lt",
+		4096,
+		[]byte{0x89,0xb6,0x9d,0x05,0x16,0xf8,0x29,0x89,0x3c,0x69,0x62,0x26,0x65,0x0a,0x86,0x87},
+	},
+}
 
-func TestWithHMAC(t *testing.T) {
-	for i, v := range testVectors {
-		o := WithHMAC(sha1.New, []byte(v.password), []byte(v.salt), v.iter, len(v.output))
+func testWithHash(t *testing.T, hash func() hash.Hash, vectors []testVector) {
+	for i, v := range vectors {
+		o := WithHMAC(hash, []byte(v.password), []byte(v.salt), v.iter, len(v.output))
 		if !bytes.Equal(o, v.output) {
 			t.Errorf("%d: expected %x, got %x", i, v.output, o)
 		}
 	}
+}
+
+func TestWithHMAC(t *testing.T) {
+	testWithHash(t, sha1.New, sha1TestVectors)
+	testWithHash(t, sha256.New, sha256TestVectors)
 }
